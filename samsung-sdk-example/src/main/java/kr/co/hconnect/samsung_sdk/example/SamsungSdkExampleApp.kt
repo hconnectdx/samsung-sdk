522f4390267a71c9b6bcc5b39c40ea122590d4c6
곽민우 <kmwdev@hconnect.co.kr>
2026-04-30 12:27:42 +0900

    Remove: 잘못만든 example 삭제

--- samsung-sdk-example/src/main/java/kr/co/hconnect/samsung_sdk/example/SamsungSdkExampleApp.kt
+++
@@ -1,7 +0,0 @@
-package kr.co.hconnect.samsung_sdk.example
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class SamsungSdkExampleApp : Application()
